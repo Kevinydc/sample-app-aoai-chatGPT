db2690096a883cefeca09d9fa731dfc7b664b5d8
Kevinydc <kevin.y@outlook.com>
2023-07-12 20:02:45 +0800

    Update Chat.tsx

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -195,7 +195,7 @@ const Chat = () => {
                                         <div className={styles.chatMessageGpt}>
                                             <Answer
                                                 answer={{
-                                                    answer: "Generating answer...",
+                                                    answer: "Generating answer.../Test",
                                                     citations: []
                                                 }}
                                                 onCitationClicked={() => null}
